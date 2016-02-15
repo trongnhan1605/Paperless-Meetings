6c60311a6535f890636031a8d020bd4a4db1d408
NguyenTrongNhan <trongnhan1605@gmail.com>
2016-02-15 10:50:26 +0700

    0001 Master branch

---
+++ PaperlessMeeting/PaperlessMeeting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaperlessMeeting
+//
+//  Created by Nguyen Nhan on 2/15/16.
+//  Copyright © 2016 Nguyen Nhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PaperlessMeeting/PaperlessMeeting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PaperlessMeeting
+//
+//  Created by Nguyen Nhan on 2/15/16.
+//  Copyright © 2016 Nguyen Nhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
